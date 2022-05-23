8bb6e3c4d0111c676745445629a2e67494979069
Sayan Mukherjee <sayanm481@gmail.com>
2022-05-23 18:28:01 +0530

    5th commit in  serverless app

--- functions/functions.js
+++ functions/functions.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context, callback) => {
-    console.log(`${event.queryStringParameters}`);
+    console.log(event.queryStringParameters);
     return {
     statusCode: 200,
     body: `subscriberID=${event.queryStringParameters.subscriberID}<br/>siteID=${event.queryStringParameters.siteID}`,
